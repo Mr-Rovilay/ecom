db265a0970c673abf76b24cde5a48270ac950b22
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-07-20 18:37:10 +0100

    hero-section

---
+++ client/src/Components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import "./Hero.css"
+import icon from "/hand_icon.png"
+import arrow from "/arrow.png"
+import text_hero from "/hero_image.png"
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="hero-left">
+            <h2>New Arrivals only</h2>
+            <div className="">
+                <div className="hero-hand-icon">
+                    <p>New</p>
+                    <img src={icon} alt="" />
+                </div>
+                <p>Collections</p>
+                <p>for everyone</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>latest collection</div>
+             <img src={arrow} alt="" />
+            </div>
+        </div>
+        <div className="hero-right">
+            <img src={text_hero} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
